f30eee4630c70237d64a7554e7a8409b1b5ac224
Zackarie Nouchet <zackarie.nouchet.auditeur@lecnam.net>
2026-09-21 12:13:53 +0200

    Rectangle

--- src/net/lecnam/ecue2/tp3/Rectangle.java
+++ src/net/lecnam/ecue2/tp3/Rectangle.java
@@ -6,17 +6,23 @@ public class Rectangle {
     double longueur2;
     Point origine;
 
-    // Constructeur 1
     public Rectangle(Point origine, double longueur1, double longueur2) {
         this.origine = origine;
         this.longueur1 = longueur1;
         this.longueur2 = longueur2;
     }
 
-    // Constructeur 2
     public Rectangle(double x, double y, double longueur1, double longueur2) {
         this.origine = new Point(x, y);
         this.longueur1 = longueur1;
         this.longueur2 = longueur2;
+    }
+
+    public double retourneSurface() {
+        return longueur1 * longueur2;
+    }
+
+    public void translate(double x, double y) {
+        origine.translate(x, y);
     }
 }
